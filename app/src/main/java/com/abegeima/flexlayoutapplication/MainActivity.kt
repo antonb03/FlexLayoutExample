0303df63e053ab66acf415fb5b1ebd80e1d04d83
Anton <anton@okcupid.com>
2020-06-16 09:30:07 -0400

    bubbles build

--- app/src/main/java/com/abegeima/flexlayoutapplication/MainActivity.kt
+++ app/src/main/java/com/abegeima/flexlayoutapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.abegeima.flexlayoutapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.abegeima.flexlayoutapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setUpListeners()
+    }
+
+    fun setUpListeners(){
+        val input = binding.inputLayout.findViewById<EditText>(R.id.input_field)
+        binding.addWord.setOnClickListener {
+            val bubbleView = LayoutInflater.from(this)
+                .inflate(R.layout.bubble_text_view, null) as TextView
+            bubbleView.text = input.text.toString()
+            bubbleView.setOnClickListener {
+                binding.container.removeView(it)
+            }
+            binding.container.addView(bubbleView)
+            input.setText("")
+        }
     }
 }
